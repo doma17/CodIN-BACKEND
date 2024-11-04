ab2f25db56400d9632008e98ddb4b8ddc6593967
kbm <rovin1273@gmail.com>
2024-11-04 17:41:40 +0900

    Chore : add basic spring application class

---
+++ src/main/java/inu/codin/codin/CodinApplication.java
@@ -0,0 +1,13 @@
+package inu.codin.codin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodinApplication.class, args);
+	}
+
+}
