d19ec52179c53b0d9c35b3dd96d1b15a051be680
kbm <rovin1273@gmail.com>
2024-11-11 20:01:05 +0900

    [SC-57] Feat : UserDetails 기능 추가

---
+++ src/main/java/inu/codin/codin/domain/user/security/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package inu.codin.codin.domain.user.security;
+
+import inu.codin.codin.domain.user.entity.UserEntity;
+import inu.codin.codin.domain.user.entity.UserStatus;
+import inu.codin.codin.domain.user.exception.UserDisabledException;
+import inu.codin.codin.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        UserEntity user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없음, email :" + email));
+
+        if (!UserStatus.ACTIVE.equals(user.getStatus())) {
+            throw new UserDisabledException("유저가 활성화되지 않았습니다");
+        }
+
+        return CustomUserDetails.from(user);
+    }
+
+}
