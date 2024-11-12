3053a3245250f18fba92d577a715329f773666d8
gimgisu <gisu1102@gmail.com>
2024-11-12 13:21:26 +0900

    [SC-67] Feat : post Create API

---
+++ src/main/java/inu/codin/codin/domain/post/exception/PostCreateFailException.java
@@ -0,0 +1,7 @@
+package inu.codin.codin.domain.post.exception;
+
+public class PostCreateFailException extends RuntimeException {
+    public PostCreateFailException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/inu/codin/codin/domain/post/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package inu.codin.codin.domain.post.repository;
+
+import inu.codin.codin.domain.post.entity.PostEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PostRepository extends MongoRepository<PostEntity, String> {
+    Optional<PostEntity> findByTitle(String title);
+}
