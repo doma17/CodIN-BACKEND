a5c02b13297ebd0e799f8df9ddecc4240ce1a5b2
kbm <rovin1273@gmail.com>
2024-11-13 20:55:33 +0900

    [SC-57] Perf : SecurityConfig 수정
    - SessionCreationPolicy -> STATLESS
    - RoleHierarchy 빈 추가

--- src/main/java/inu/codin/codin/common/config/SecurityConfig.java
+++ src/main/java/inu/codin/codin/common/config/SecurityConfig.java
@@ -2,12 +2,14 @@ package inu.codin.codin.common.config;
 
 import inu.codin.codin.common.security.filter.ExceptionHandlerFilter;
 import inu.codin.codin.common.security.filter.JwtAuthenticationFilter;
+import inu.codin.codin.common.security.jwt.JwtTokenProvider;
 import inu.codin.codin.common.security.jwt.JwtUtils;
 import inu.codin.codin.common.security.service.JwtService;
-import inu.codin.codin.common.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -16,11 +18,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -32,20 +36,6 @@ public class SecurityConfig {
     private final JwtService jwtService;
     private final JwtUtils jwtUtils;
 
-    String[] PERMIT_ALL = {
-            "/api/login",
-            "/api/sign-up",
-            "/api/reissue-token",
-            "/api/logout",
-            "/api/members/sign-up",
-            "/api/members/sign-in",
-            "/api/members/refresh-token",
-            "/api/swagger-ui/**",
-            "/api/swagger-ui.html",
-            "/api/v3/api-docs/**",
-            "/**"
-    };
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
@@ -54,10 +44,15 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults()) // cors 설정
                 .formLogin(FormLoginConfigurer::disable) // form login 비활성화
                 .httpBasic(HttpBasicConfigurer::disable) // basic auth 비활성화
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용하지 않음
+                )
                 // authorizeHttpRequests 메서드를 통해 요청에 대한 권한 설정
                 .authorizeHttpRequests((authorizeHttpRequests) ->
                         authorizeHttpRequests
                                 .requestMatchers(PERMIT_ALL).permitAll()
+                                .requestMatchers(SWAGGER_AUTH_PATHS).permitAll() //.hasRole("ADMIN")
+                                .anyRequest().hasRole("USER")
                 )
                 // JwtAuthenticationFilter 추가
                 .addFilterBefore(
@@ -65,7 +60,7 @@ public class SecurityConfig {
                         UsernamePasswordAuthenticationFilter.class
                 )
                 // 예외 처리 필터 추가
-                .addFilterBefore(new ExceptionHandlerFilter(), JwtAuthenticationFilter.class);
+                .addFilterBefore(new ExceptionHandlerFilter(), LogoutFilter.class);
 
         return http.build();
     }
@@ -78,7 +73,38 @@ public class SecurityConfig {
     }
 
     @Bean
+    public RoleHierarchy roleHierarchy() {
+        return RoleHierarchyImpl.fromHierarchy("ROLE_ADMIN > ROLE_MANGER > ROLE_USER");
+    }
+
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    private static final String[] PERMIT_ALL = {
+            // Production Level
+            "/api/auth/login",
+            "/api/auth/reissue",
+            "/api/auth/logout",
+            "/api/users/sign-up",
+            "/api/email/auth/check",
+            "/api/email/auth/send",
+
+            // Local Test Level
+            "/auth/login",
+            "/auth/reissue",
+            "/auth/logout",
+            "/users/sign-up",
+            "/email/auth/check",
+            "/email/auth/send"
+    };
+
+    private static final String[] SWAGGER_AUTH_PATHS = {
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/v3/api-docs",
+            "/swagger-resources/**",
+    };
+
 }
