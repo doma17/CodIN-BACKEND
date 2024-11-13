10c6294fdfd614174e376779dd4f542c55710ecc
kbm <rovin1273@gmail.com>
2024-11-13 15:41:07 +0900

    [SC-57] Feat : RedisConfig 수정

--- src/main/java/inu/codin/codin/infra/redis/RedisConfig.java
+++ src/main/java/inu/codin/codin/infra/redis/RedisConfig.java
@@ -5,8 +5,10 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 // Redis configuration
@@ -19,16 +21,26 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
+
+        RedisStandaloneConfiguration redisStandAloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandAloneConfiguration.setPort(redisProperties.getPort());
+        redisStandAloneConfiguration.setHostName(redisProperties.getHost());
+        redisStandAloneConfiguration.setPassword(redisProperties.getPassword());
+        redisStandAloneConfiguration.setDatabase(0);
+
         // Lettuce는 비동기 방식을 지원하는 Redis 클라이언트
         // 성능상 이점이 있어 기본적으로 사용
-        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+        return new LettuceConnectionFactory(redisStandAloneConfiguration);
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setDefaultSerializer(RedisSerializer.string());
+        redisTemplate.setEnableTransactionSupport(true);
         return redisTemplate;
     }
 }
