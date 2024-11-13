f0e2d022318b0efb2a74aee8d7b083e78145f452
kbm <rovin1273@gmail.com>
2024-11-13 15:48:24 +0900

    [SC-57] Test : 유저 컨텍스트 테스트용 컨트롤러 메서드 추가

--- src/main/java/inu/codin/codin/TestController.java
+++ src/main/java/inu/codin/codin/TestController.java
@@ -1,5 +1,7 @@
 package inu.codin.codin;
 
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v3/api")
 public class TestController {
 
+    @Operation(summary = "기본 접근 테스트")
     @GetMapping("/test")
-    public String test() {
+    public String test1() {
         return "test";
+    }
+
+    @Operation(summary = "기본 접근 테스트 - 로그인 데이터 확인")
+    @GetMapping("/test2")
+    public String test2() {
+        // SecurityContextHolder를 이용하여 현재 사용자의 정보를 가져올 수 있습니다.
+        String username = SecurityContextHolder // SecurityContextHolder를 이용하여 현재 사용자의 정보를 가져올 수 있습니다.
+                .getContext()
+                .getAuthentication()
+                .getName();
+        return "유저 이름: " + username;
     }
 
 }
