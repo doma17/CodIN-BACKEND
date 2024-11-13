e77a1e5a3138a8a31017f930eccb2829e9f23923
X1n9fU <minkyeong0244@gmail.com>
2024-11-13 17:08:26 +0900

    [SC-71] docs : @Operation 추가

--- src/main/java/inu/codin/codin/domain/info/office/controller/OfficeController.java
+++ src/main/java/inu/codin/codin/domain/info/office/controller/OfficeController.java
@@ -2,6 +2,7 @@ package inu.codin.codin.domain.info.office.controller;
 
 import inu.codin.codin.common.Department;
 import inu.codin.codin.domain.info.office.service.OfficeService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,13 +17,14 @@ public class OfficeController {
 
     private final OfficeService officeService;
 
+    @Operation(summary = "학과별 사무실 직원 정보 반환")
     @GetMapping("/{department}")
     public ResponseEntity<?> getOfficeByDepartment(@PathVariable("department") Department department){
         return ResponseEntity.ok()
                 .body(officeService.getOfficeByDepartment(department));
     }
 
-
+    @Operation(summary = "학과사무실 리스트 반환")
     @GetMapping
     public ResponseEntity<?> getAllOffice(){
         return ResponseEntity.ok()
