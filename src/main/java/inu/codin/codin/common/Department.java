3e5aa3ca0f060b4f7238ba0d7dcfe7f08945bbe5
kbm <rovin1273@gmail.com>
2024-11-17 02:52:58 +0900

    [SC-71] Refactor : Department 통합

--- src/main/java/inu/codin/codin/common/Department.java
+++ src/main/java/inu/codin/codin/common/Department.java
@@ -1,8 +1,18 @@
 package inu.codin.codin.common;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public enum Department {
-    CSE, ITE, ESE, COLLEGE
+
+    IT_COLLEGE("정보기술대학"),
+    COMPUTER_SCI("컴퓨터공학과"),
+    INFO_COMM("정보통신공학과"),
+    EMBEDDED("임베디드시스템공학과"),
+    STAFF("교직원");
+
+    private final String description;
+
 }
---
+++ src/main/java/inu/codin/codin/domain/user/entity/UserEntity.java
@@ -0,0 +1,48 @@
+package inu.codin.codin.domain.user.entity;
+
+import inu.codin.codin.common.BaseTimeEntity;
+import inu.codin.codin.common.Department;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+@Getter
+public class UserEntity extends BaseTimeEntity {
+
+    @Id @NotBlank
+    private String id;
+
+    private String email;
+
+    private String password;
+
+    private String studentId;
+
+    private String name;
+
+    private String nickname;
+
+    private String profileImageUrl;
+
+    private Department department;
+
+    private UserRole role;
+
+    private UserStatus status;
+
+    @Builder
+    public UserEntity(String email, String password, String studentId, String name, String nickname, String profileImageUrl, Department department, UserRole role, UserStatus status) {
+        this.email = email;
+        this.password = password;
+        this.studentId = studentId;
+        this.name = name;
+        this.nickname = nickname;
+        this.profileImageUrl = profileImageUrl;
+        this.department = department;
+        this.role = role;
+        this.status = status;
+    }
+}
