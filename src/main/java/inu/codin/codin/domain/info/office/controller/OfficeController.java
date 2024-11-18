03f90ce8796914ad28dd937f30f74ce81f4d664e
X1n9fU <minkyeong0244@gmail.com>
2024-11-18 12:11:56 +0900

    [SC-71] refactor : Response 명시

--- src/main/java/inu/codin/codin/domain/info/office/controller/OfficeController.java
+++ src/main/java/inu/codin/codin/domain/info/office/controller/OfficeController.java
@@ -1,6 +1,8 @@
 package inu.codin.codin.domain.info.office.controller;
 
 import inu.codin.codin.common.Department;
+import inu.codin.codin.domain.info.office.dto.OfficeListResDTO;
+import inu.codin.codin.domain.info.office.dto.OfficeMemberResDTO;
 import inu.codin.codin.domain.info.office.service.OfficeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,14 +25,14 @@ public class OfficeController {
 
     @Operation(summary = "학과별 사무실 직원 정보 반환")
     @GetMapping("/{department}")
-    public ResponseEntity<?> getOfficeByDepartment(@PathVariable("department") Department department){
+    public ResponseEntity<List<OfficeMemberResDTO>> getOfficeByDepartment(@PathVariable("department") Department department){
         return ResponseEntity.ok()
                 .body(officeService.getOfficeByDepartment(department));
     }
 
     @Operation(summary = "학과사무실 리스트 반환")
     @GetMapping
-    public ResponseEntity<?> getAllOffice(){
+    public ResponseEntity<List<OfficeListResDTO>> getAllOffice(){
         return ResponseEntity.ok()
                 .body(officeService.getAllOffice());
     }
