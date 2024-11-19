8ad8ad70ce18655633befe2f2c3ad5176613196d
gimgisu <gisu1102@gmail.com>
2024-11-19 14:51:49 +0900

    Revert "Merge pull request #17 from CodIN-INU/SC-67-post-create"
    This reverts commit 4983a930d931b17e303e8f60beb4afeb13cc56a0, reversing
    changes made to ab6b2c00f0305e254332f2c7c209ddd540d7d6c4.

--- src/main/java/inu/codin/codin/domain/post/exception/PostCreateFailException.java
+++
@@ -1,7 +0,0 @@
-package inu.codin.codin.domain.post.exception;
-
-public class PostCreateFailException extends RuntimeException {
-    public PostCreateFailException(String message) {
-        super(message);
-    }
-}
