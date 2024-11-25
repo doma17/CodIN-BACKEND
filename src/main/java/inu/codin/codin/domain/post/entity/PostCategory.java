5ee7c90729eb9649bbf18a18604ed2ac5c3363e4
gimgisu <gisu1102@gmail.com>
2024-11-25 14:06:07 +0900

    [SC-67]  Refactor : 카테고리 ENUM 세부내용 변경

--- src/main/java/inu/codin/codin/domain/post/entity/PostCategory.java
+++ src/main/java/inu/codin/codin/domain/post/entity/PostCategory.java
@@ -6,10 +6,21 @@ import lombok.Getter;
 
 @Getter
 public enum PostCategory {
-    REQUEST("구해요"),
-    COMMUNICATION("소통해요"),
-    EXTRACURRICULAR("비교과"),
-    INFO("정보대소개"),
+    REQUEST_STUDY("구해요_스터디"),
+    REQUEST_PROJECT("구해요_프로젝트"),
+    REQUEST_COMPETITION("구해요_공모전_대회"),
+    REQUEST_GROUP("구해요_소모임"),
+
+    COMMUNICATION_QUESTION("소통해요_질문"),
+    COMMUNICATION_JOB("소통해요_취업수기"),
+    COMMUNICATION_TIP("소통해요_꿀팁공유"),
+
+    EXTRACURRICULAR_OUTER("비교과_교외"),
+    EXTRACURRICULAR_INNER("비교과_교내"),
+
+    INFO_PROFESSOR("정보대소개_교수_연구실"),
+    INFO_PHONE_NUMBER("정보대소개_정보대전화번호"),
+
     USED_BOOK("중고책");
 
     private final String description;
