28d8107b002d341de879557aad267e154228f15f
gimgisu <gisu1102@gmail.com>
2024-11-25 13:27:28 +0900

    [SC-69]  Refactor : Api 수정

--- src/main/java/inu/codin/codin/domain/like/LikeController.java
+++ src/main/java/inu/codin/codin/domain/like/LikeController.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/posts/{postId}/likes")
+@RequestMapping("/api/likes/{postId}")
 @RequiredArgsConstructor
 public class LikeController {
 
