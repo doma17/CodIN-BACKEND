4b644e3519d1767c1c256f1460eda7e7e027a6b8
kbm <rovin1273@gmail.com>
2024-11-26 22:30:26 +0900

    [SC-64] Feat : FcmMessageDto에 ImageUrl 첨부기능 추가

--- src/main/java/inu/codin/codin/infra/fcm/dto/FcmMessageDto.java
+++ src/main/java/inu/codin/codin/infra/fcm/dto/FcmMessageDto.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 
+import java.util.Map;
+
 /**
  * Fcm 메시지 DTO
  * 서버 내부 로직에서 사용
@@ -16,11 +18,15 @@ public class FcmMessageDto {
     private UserEntity user;
     private String title;
     private String body;
+    private String imageUrl;
+    private Map<String, String> data;
 
     @Builder
-    public FcmMessageDto(UserEntity user, String title, String body) {
+    public FcmMessageDto(UserEntity user, String title, String body, String imageUrl, Map<String, String> data) {
         this.user = user;
         this.title = title;
         this.body = body;
+        this.imageUrl = imageUrl;
+        this.data = data;
     }
 }
