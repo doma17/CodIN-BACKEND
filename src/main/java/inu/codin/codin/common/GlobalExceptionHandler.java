9090fced41bcb9b3581d6a6b156262f13119c0dd
X1n9fU <minkyeong0244@gmail.com>
2024-11-27 17:33:02 +0900

    refactor : ExceptionResponse 설정

--- src/main/java/inu/codin/codin/common/GlobalExceptionHandler.java
+++ src/main/java/inu/codin/codin/common/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package inu.codin.codin.common;
 
+import inu.codin.codin.common.response.ExceptionResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,8 +10,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    protected ResponseEntity<?> handleException(Exception e) {
-        return ResponseUtils.error(e.getMessage());
+    protected ResponseEntity<ExceptionResponse> handleException(Exception e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ExceptionResponse(e.getMessage(), HttpStatus.NOT_FOUND.value()));
     }
 
 }
