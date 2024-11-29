9705fc9206772e800d64e78ae8f5305e105f9151
gimgisu <gisu1102@gmail.com>
2024-11-30 03:10:22 +0900

    [SC-68]  Refactor : Response 통일

--- src/main/java/inu/codin/codin/domain/post/comment/controller/CommentController.java
+++ src/main/java/inu/codin/codin/domain/post/comment/controller/CommentController.java
@@ -1,5 +1,7 @@
 package inu.codin.codin.domain.post.comment.controller;
 
+import inu.codin.codin.common.response.ListResponse;
+import inu.codin.codin.common.response.SingleResponse;
 import inu.codin.codin.domain.post.dto.request.CommentCreateRequestDTO;
 import inu.codin.codin.domain.post.dto.request.ReplyCreateRequestDTO;
 import inu.codin.codin.domain.post.dto.response.CommentsResponseDTO;
@@ -23,38 +25,45 @@ public class CommentController {
 
     @Operation(summary = "댓글 추가")
     @PostMapping("/{postId}")
-    public ResponseEntity<String> addComment(@PathVariable String postId,
+    public ResponseEntity<SingleResponse<?>> addComment(@PathVariable String postId,
                                              @RequestBody @Valid CommentCreateRequestDTO requestDTO) {
         commentService.addComment(postId, requestDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body("댓글이 추가되었습니다.");
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new SingleResponse<>(201, "댓글이 추가되었습니다.", null));
     }
 
     @Operation(summary = "대댓글 추가")
     @PostMapping("/{commentId}/replies")
-    public ResponseEntity<String> addReply(@PathVariable String commentId,
+    public ResponseEntity<SingleResponse<?>> addReply(@PathVariable String commentId,
                                            @RequestBody @Valid ReplyCreateRequestDTO requestDTO) {
         commentService.addReply(commentId, requestDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body("대댓글이 추가되었습니다.");
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new SingleResponse<>(201, "대댓글이 추가되었습니다.", null));
+
     }
 
     @Operation(summary = "해당 게시물의 댓글 및 대댓글 조회")
     @GetMapping("/post/{postId}")
-    public ResponseEntity<List<CommentsResponseDTO>> getCommentsByPostId(@PathVariable String postId) {
+    public ResponseEntity<ListResponse<CommentsResponseDTO>> getCommentsByPostId(@PathVariable String postId) {
         List<CommentsResponseDTO> response = commentService.getCommentsByPostId(postId);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok()
+                .body(new ListResponse<>(200, "해당 게시물의 댓글 및 대댓글 조회 성공", response));
+
     }
 
     @Operation(summary = "댓글 삭제")
     @DeleteMapping("/{commentId}")
-    public ResponseEntity<String> deleteComment(@PathVariable String commentId) {
+    public ResponseEntity<SingleResponse<?>> deleteComment(@PathVariable String commentId) {
         commentService.deleteComment(commentId);
-        return ResponseEntity.status(HttpStatus.OK).body("댓글이 삭제되었습니다.");
+        return ResponseEntity.ok()
+                .body(new SingleResponse<>(200, "댓글이 삭제되었습니다.", null));
     }
 
     @Operation(summary = "대댓글 삭제")
     @DeleteMapping("/replies/{replyId}")
-    public ResponseEntity<String> deleteReply(@PathVariable String replyId) {
+    public ResponseEntity<SingleResponse<?>> deleteReply(@PathVariable String replyId) {
         commentService.deleteReply(replyId);
-        return ResponseEntity.status(HttpStatus.OK).body("대댓글이 삭제되었습니다.");
+        return ResponseEntity.ok()
+                .body(new SingleResponse<>(200, "대댓글이 삭제되었습니다.", null));
     }
 }
--- src/main/java/inu/codin/codin/domain/post/controller/PostController.java
+++ src/main/java/inu/codin/codin/domain/post/controller/PostController.java
@@ -1,5 +1,7 @@
 package inu.codin.codin.domain.post.controller;
 
+import inu.codin.codin.common.response.ListResponse;
+import inu.codin.codin.common.response.SingleResponse;
 import inu.codin.codin.domain.post.dto.request.PostAnonymousUpdateRequestDTO;
 import inu.codin.codin.domain.post.dto.request.PostContentUpdateRequestDTO;
 import inu.codin.codin.domain.post.dto.request.PostCreateRequestDTO;
@@ -32,7 +34,7 @@ public class PostController {
             description = "JSON 형식의 게시물 데이터(postContent)와 이미지 파일(postImages) 업로드"
     )
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<String> createPost(
+    public ResponseEntity<SingleResponse<?>>  createPost(
             @RequestPart("postContent") @Valid PostCreateRequestDTO postCreateRequestDTO,
             @RequestPart(value = "postImages", required = false) List<MultipartFile> postImages) {
 
@@ -42,41 +44,45 @@ public class PostController {
         }
 
         postService.createPost(postCreateRequestDTO, postImages);
-        return ResponseEntity.status(HttpStatus.CREATED).body("게시물 작성 성공");
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new SingleResponse<>(201, "게시물이 작성되었습니다.", null));
     }
 
     @Operation(
             summary = "게시물 내용 이미지 수정 추가"
     )
     @PatchMapping(value = "/{postId}/content", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<String> updatePostContent(
+    public ResponseEntity<SingleResponse<?>>  updatePostContent(
             @PathVariable String postId,
             @RequestPart("postContent") @Valid PostContentUpdateRequestDTO requestDTO,
             @RequestPart(value = "postImages", required = false) List<MultipartFile> postImages) {
 
         postService.updatePostContent(postId, requestDTO, postImages);
-        return ResponseEntity.status(HttpStatus.OK).body("게시물 수정 성공");
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new SingleResponse<>(200, "게시물 내용이 수정되었습니다.", null));
     }
 
     @Operation(
             summary = "상태 수정"
     )
     @PatchMapping("/{postId}/status")
-    public ResponseEntity<String> updatePostStatus(
+    public ResponseEntity<SingleResponse<?>> updatePostStatus(
             @PathVariable String postId,
             @RequestBody PostStatusUpdateRequestDTO requestDTO) {
         postService.updatePostStatus(postId, requestDTO);
-        return ResponseEntity.status(HttpStatus.OK).body("게시물 상태 수정 성공");
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new SingleResponse<>(200, "게시물 상태가 수정되었습니다.", null));
     }
 
 
     @Operation(summary = "게시물 익명 설정 수정")
     @PatchMapping("/{postId}/anonymous")
-    public ResponseEntity<String> updatePostAnonymous(
+    public ResponseEntity<SingleResponse<?>> updatePostAnonymous(
             @PathVariable String postId,
             @RequestBody @Valid PostAnonymousUpdateRequestDTO requestDTO) {
         postService.updatePostAnonymous(postId, requestDTO);
-        return ResponseEntity.status(HttpStatus.OK).body("게시물 익명 설정이 수정되었습니다.");
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new SingleResponse<>(200, "게시물 익명 설정이 수정되었습니다.", null));
     }
 
 
@@ -84,48 +90,46 @@ public class PostController {
             summary = "삭제되지 않은 모든 게시물 조회"
     )
     @GetMapping("")
-    public ResponseEntity<List<PostWithCountsResponseDTO>> getAllPosts() {
+    public ResponseEntity<ListResponse<PostWithCountsResponseDTO>> getAllPosts() {
         List<PostWithCountsResponseDTO> posts = postService.getAllPosts();
-        return ResponseEntity.ok(posts);
+        return ResponseEntity.ok()
+                .body(new ListResponse<>(200, "삭제되지 않은 모든 게시물 조회 성공", posts));
     }
 
     @Operation(
             summary = "해당 사용자 게시물 전체 조회"
     )
     @GetMapping("/user/{userId}")
-    public ResponseEntity<List<PostWithCountsResponseDTO>> getAllUserPosts(@PathVariable String userId) {
+    public ResponseEntity<ListResponse<PostWithCountsResponseDTO>> getAllUserPosts(@PathVariable String userId) {
         List<PostWithCountsResponseDTO> posts = postService.getAllUserPosts(userId);
-        return ResponseEntity.ok(posts);
+        return ResponseEntity.ok()
+                .body(new ListResponse<>(200, "사용자 게시물 조회 성공", posts));
     }
 
-    @Operation(
-            summary = "해당 게시물 상세 조회"
-    )
+    @Operation(summary = "해당 게시물 상세 조회")
     @GetMapping("/{postId}")
-    public ResponseEntity<PostWithDetailResponseDTO> getPostWithDetail(@PathVariable String postId) {
+    public ResponseEntity<SingleResponse<PostWithDetailResponseDTO>> getPostWithDetail(@PathVariable String postId) {
         PostWithDetailResponseDTO post = postService.getPostWithDetail(postId);
-        return ResponseEntity.ok(post);
+        return ResponseEntity.ok()
+                .body(new SingleResponse<>(200, "게시물 상세 조회 성공", post));
     }
-
 
     @Operation(summary = "게시물 이미지 삭제")
     @DeleteMapping("/{postId}/images")
-    public ResponseEntity<String> deletePostImage(
+    public ResponseEntity<SingleResponse<?>> deletePostImage(
             @PathVariable String postId,
             @RequestParam String imageUrl) {
+
         postService.deletePostImage(postId, imageUrl);
-        return ResponseEntity.status(HttpStatus.OK).body("이미지가 삭제되었습니다.");
+        return ResponseEntity.ok()
+                .body(new SingleResponse<>(200, "게시물 이미지가 삭제되었습니다.", null));
     }
 
     @Operation(summary = "게시물 삭제 (Soft Delete)")
     @DeleteMapping("/{postId}")
-    public ResponseEntity<String> softDeletePost(@PathVariable String postId) {
+    public ResponseEntity<SingleResponse<?>> softDeletePost(@PathVariable String postId) {
         postService.softDeletePost(postId);
-        return ResponseEntity.status(HttpStatus.OK).body("게시물이 삭제되었습니다.");
+        return ResponseEntity.ok()
+                .body(new SingleResponse<>(200, "게시물이 삭제되었습니다.", null));
     }
-
-
-
-
 }
-
