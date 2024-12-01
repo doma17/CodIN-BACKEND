232e7e5d91b5077bf37e3e33255855002ebf49c9
X1n9fU <minkyeong0244@gmail.com>
2024-12-01 14:32:41 +0900

    refactor : 추후 1:N을 위한 STOMP 구독 형식 변경

---
+++ src/main/java/inu/codin/codin/domain/chat/chatting/entity/MessageType.java
@@ -0,0 +1,10 @@
+package inu.codin.codin.domain.chat.chatting.entity;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum MessageType {
+    SEND,
+    EXIT
+    //추후 추가 예정
+}
