555fb1cd671cfb0810d6b5f7a78c4d387527520d
X1n9fU <minkyeong0244@gmail.com>
2024-12-02 17:46:31 +0900

    [SC-65] feat : NotFoundException common으로 제작

---
+++ src/main/java/inu/codin/codin/common/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package inu.codin.codin.common.exception;
+
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
