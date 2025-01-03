2b579b79b3de0d06c5a63ad978488bd9d69bbf90
X1n9fU <minkyeong0244@gmail.com>
2025-01-04 02:30:31 +0900

    fix : setExposedHeaders에 x-refresh-token 추가

--- src/main/java/inu/codin/codin/common/config/SecurityConfig.java
+++ src/main/java/inu/codin/codin/common/config/SecurityConfig.java
@@ -154,7 +154,7 @@ public class SecurityConfig {
         config.setAllowedOrigins(List.of("http://localhost:3000", "https://www.codin.co.kr"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
-        config.addExposedHeader("Authorization");
+        config.setExposedHeaders(List.of("Authorization", "x-refresh-token"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return source;
