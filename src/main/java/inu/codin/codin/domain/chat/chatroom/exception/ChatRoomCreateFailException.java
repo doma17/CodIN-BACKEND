8b82e517bbbc4fa257478c09c189cafd95f23924
X1n9fU <minkyeong0244@gmail.com>
2025-01-15 23:39:34 +0900

    fix : 자신과의 채팅방 생성 불가

---
+++ src/main/java/inu/codin/codin/domain/chat/chatroom/exception/ChatRoomCreateFailException.java
@@ -0,0 +1,7 @@
+package inu.codin.codin.domain.chat.chatroom.exception;
+
+public class ChatRoomCreateFailException extends RuntimeException{
+    public ChatRoomCreateFailException(String message) {
+        super(message);
+    }
+}
