66771a6a097cc1d090eb2bb331264a1dcbfe5f5e
X1n9fU <minkyeong0244@gmail.com>
2025-01-21 01:02:31 +0900

    feat : AES 암호화 설정 추가

---
+++ src/main/java/inu/codin/codin/common/util/AESUtil.java
@@ -0,0 +1,39 @@
+package inu.codin.codin.common.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+@Component
+public class AESUtil {
+
+    private static final String ALGORITHM = "AES";
+
+    private static String SECRET_KEY; // 반드시 16, 24, 또는 32바이트여야 합니다.
+
+    @Autowired
+    public AESUtil(Environment environment) {
+        SECRET_KEY = environment.getProperty("user.sign_up.commSessionKEY");
+    }
+
+    public static String encrypt(String input) throws Exception {
+        SecretKeySpec keySpec = new SecretKeySpec(SECRET_KEY.getBytes(), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec);
+        byte[] encryptedBytes = cipher.doFinal(input.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+
+    public static String decrypt(String encryptedInput) throws Exception {
+        SecretKeySpec keySpec = new SecretKeySpec(SECRET_KEY.getBytes(), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, keySpec);
+        byte[] encryptedBytes = Base64.getDecoder().decode(encryptedInput);
+        byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
+        return new String(decryptedBytes);
+    }
+}
