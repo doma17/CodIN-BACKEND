178bfee97fd94739a68ba1bdb66d81033c21a638
X1n9fU <minkyeong0244@gmail.com>
2025-01-21 22:54:04 +0900

    perf : 유저 회원가입 수정 및 비밀번호 암호화 설정

--- src/main/java/inu/codin/codin/common/security/controller/AuthController.java
+++ src/main/java/inu/codin/codin/common/security/controller/AuthController.java
@@ -4,21 +4,21 @@ import inu.codin.codin.common.response.SingleResponse;
 import inu.codin.codin.common.security.dto.SignUpAndLoginRequestDto;
 import inu.codin.codin.common.security.service.AuthService;
 import inu.codin.codin.common.security.service.JwtService;
+import inu.codin.codin.domain.user.dto.request.UserNicknameRequestDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping(value = "/auth")
@@ -68,5 +68,16 @@ public class AuthController {
         authService.signUp(signUpAndLoginRequestDto);
         return ResponseEntity.ok()
                 .body(new SingleResponse<>(200, "포탈 로그인을 통한 학적 정보 반환 완료", null));
+    }
+
+    @Operation(summary = "회원가입")
+    @PostMapping(value = "/signup/{studentId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<SingleResponse<?>> signUpUser(
+            @PathVariable("studentId") String studentId,
+            @RequestPart @Valid UserNicknameRequestDto userNicknameRequestDto,
+            @RequestPart(value = "userImage", required = false) MultipartFile userImage) {
+        authService.createUser(studentId, userNicknameRequestDto, userImage);
+        return ResponseEntity.ok()
+                .body(new SingleResponse<>(200, "회원가입 성공", null));
     }
 }
--- src/main/java/inu/codin/codin/common/security/service/AuthService.java
+++ src/main/java/inu/codin/codin/common/security/service/AuthService.java
@@ -1,21 +1,27 @@
 package inu.codin.codin.common.security.service;
 
 import inu.codin.codin.common.Department;
+import inu.codin.codin.common.security.dto.PortalLoginResponseDto;
+import inu.codin.codin.common.security.dto.SignUpAndLoginRequestDto;
 import inu.codin.codin.common.security.feign.inu.InuClient;
 import inu.codin.codin.common.security.feign.portal.PortalClient;
-import inu.codin.codin.common.security.dto.PortalLoginResponseDto;
 import inu.codin.codin.common.util.AESUtil;
-import inu.codin.codin.common.security.dto.SignUpAndLoginRequestDto;
+import inu.codin.codin.domain.user.dto.request.UserNicknameRequestDto;
 import inu.codin.codin.domain.user.entity.UserEntity;
 import inu.codin.codin.domain.user.exception.UserCreateFailException;
 import inu.codin.codin.domain.user.repository.UserRepository;
+import inu.codin.codin.infra.s3.S3Service;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Base64;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -23,11 +29,14 @@ import static feign.Util.ISO_8859_1;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AuthService {
 
     private final PortalClient portalClient;
     private final InuClient inuClient;
     private final UserRepository userRepository;
+    private final S3Service s3Service;
+    private final PasswordEncoder passwordEncoder;
 
     public void signUp(SignUpAndLoginRequestDto signUpAndLoginRequestDto) throws Exception {
         //학번으로 회원가입 유무 판단
@@ -41,15 +50,15 @@ public class AuthService {
     }
 
     private PortalLoginResponseDto returnPortalInfo(SignUpAndLoginRequestDto signUpAndLoginRequestDto) throws Exception {
-        String password = AESUtil.encrypt(signUpAndLoginRequestDto.getPassword());
         String html = portalClient.signUp(
                 "submit",
                 AESUtil.encrypt(signUpAndLoginRequestDto.getStudentId()),
-                password
+                AESUtil.encrypt(signUpAndLoginRequestDto.getPassword())
         );
         Document doc = Jsoup.parse(html);
         String value = doc.select(".main").select("input[type=hidden]").attr("value");
         PortalLoginResponseDto userPortalLoginResponseDto = readJson(value);
+        String password = passwordEncoder.encode(signUpAndLoginRequestDto.getPassword());
         userPortalLoginResponseDto.setPassword(password);
         userPortalLoginResponseDto.setUndergraduate(isUnderGraduate(signUpAndLoginRequestDto));
         return userPortalLoginResponseDto;
@@ -85,5 +94,31 @@ public class AuthService {
         String basic = "Basic " + Base64.getEncoder().encodeToString((signUpAndLoginRequestDto.getStudentId() + ":" + signUpAndLoginRequestDto.getPassword()).getBytes(ISO_8859_1));
         Map<String, String> graduate = inuClient.status(basic);
         return graduate.get("undergraduate").equals("true");
+    }
+
+    public void createUser(String studentId, UserNicknameRequestDto userNicknameRequestDto, MultipartFile userImage) {
+
+        UserEntity user = userRepository.findByStudentId(studentId)
+                        .orElseThrow(() -> new UserCreateFailException("존재하지 않는 학번입니다. 포탈 로그인부터 진행해주세요."));
+        log.info("[createUser] 요청 데이터: {}", studentId);
+
+        String imageUrl = null;
+        if (userImage != null) {
+            log.info("[회원가입] 프로필 이미지 업로드 중...");
+            imageUrl = s3Service.handleImageUpload(List.of(userImage)).get(0);
+            log.info("[회원가입] 프로필 이미지 업로드 완료: {}", imageUrl);
+        }
+
+        // imageUrl이 null이면 기본 이미지로 설정
+        if (imageUrl == null) {
+            imageUrl = s3Service.getDefaultProfileImageUrl(); // S3Service에서 기본 이미지 URL 가져오기
+
+        }
+
+        user.updateNickname(new UserNicknameRequestDto(userNicknameRequestDto.getNickname()));
+        user.updateProfileImageUrl(imageUrl);
+        userRepository.save(user);
+
+        log.info("[signUpUser] SIGN UP SUCCESS!! : {}", user.getStudentId());
     }
 }
