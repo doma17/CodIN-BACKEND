ab51508028ccdfacfd84e1e26ab34260f1dc4f94
X1n9fU <minkyeong0244@gmail.com>
2025-01-29 00:00:01 +0900

    pref : 층마다가 아닌 한 번에 강의실 현황 반환

--- src/main/java/inu/codin/codin/domain/lecture/domain/room/controller/LectureRoomController.java
+++ src/main/java/inu/codin/codin/domain/lecture/domain/room/controller/LectureRoomController.java
@@ -4,13 +4,10 @@ import inu.codin.codin.common.response.SingleResponse;
 import inu.codin.codin.domain.lecture.domain.room.service.LectureRoomService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/rooms")
@@ -26,8 +23,8 @@ public class LectureRoomController {
             description = "당일의 요일에 따라 층마다 호실에서의 수업 내용 반환"
     )
     @GetMapping("/empty")
-    public ResponseEntity<?> statusOfEmptyRoom(@RequestParam("floor") @Max(5) @Min(1) int floor){
+    public ResponseEntity<?> statusOfEmptyRoom(){
         return ResponseEntity.ok()
-                .body(new SingleResponse<>(200, floor + "층의 강의실 현황 반환", lectureRoomService.statusOfEmptyRoom(floor)));
+                .body(new SingleResponse<>(200, "오늘의 강의실 현황 반환", lectureRoomService.statusOfEmptyRoom()));
     }
 }
