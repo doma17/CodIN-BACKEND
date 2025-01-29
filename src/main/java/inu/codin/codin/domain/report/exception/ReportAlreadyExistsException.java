bb9f005d0ab73e0653dbd3cb42d6a31bfae78d3b
gimgisu <gisu1102@gmail.com>
2025-01-29 17:32:53 +0900

    feat :: 예외처리 추가

---
+++ src/main/java/inu/codin/codin/domain/report/exception/ReportAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package inu.codin.codin.domain.report.exception;
+
+public class ReportAlreadyExistsException extends RuntimeException {
+    public ReportAlreadyExistsException(String message) {
+        super(message);
+    }
+}
+
