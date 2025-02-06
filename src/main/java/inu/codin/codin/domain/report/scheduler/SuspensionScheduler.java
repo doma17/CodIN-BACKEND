8e7351b5887b9d7bf8613bc895fb82d0d99f8cfb
gimgisu <gisu1102@gmail.com>
2025-02-06 15:41:53 +0900

    refactor :: 신고 / 정지 해제 스케줄러 실행 시간 -> 1시간 마다

--- src/main/java/inu/codin/codin/domain/report/scheduler/SuspensionScheduler.java
+++ src/main/java/inu/codin/codin/domain/report/scheduler/SuspensionScheduler.java
@@ -13,8 +13,8 @@ public class SuspensionScheduler {
 
     private final SuspensionService suspensionService;
 
-    //@Scheduled(cron = "0 0 * * * ?") // 매 정시마다 실행
-    @Scheduled(cron = "0 * * * * ?") // 매 1분마다 실행
+    @Scheduled(cron = "0 0 * * * ?") // 매 정시마다 실행
+    //@Scheduled(cron = "0 * * * * ?") // 매 1분마다 실행(테스트)
     public void checkAndReleaseSuspendedUsers() {
         log.info("정지 해제 스케줄러 실행...");
         suspensionService.releaseSuspendedUsers();
