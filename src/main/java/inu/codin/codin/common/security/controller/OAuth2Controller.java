0254fd6ddefbef710ab629e3d5bc86d36f1459e7
X1n9fU <minkyeong0244@gmail.com>
2025-02-22 18:02:52 +0900

    fix : redirect-uri 수정

--- src/main/java/inu/codin/codin/common/security/controller/OAuth2Controller.java
+++ src/main/java/inu/codin/codin/common/security/controller/OAuth2Controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/login/oauth2/code")
+@RequestMapping("/login/oauth2/code")
 public class OAuth2Controller {
 
     @GetMapping("/google")
