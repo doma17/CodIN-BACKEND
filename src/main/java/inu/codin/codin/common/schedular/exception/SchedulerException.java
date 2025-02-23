2e0898a886e35a6630822ee2c2779df84f5d1f5a
X1n9fU <minkyeong0244@gmail.com>
2025-02-24 01:08:32 +0900

    feat: 비교과 게시글 스케줄러로 자동 업데이트

---
+++ src/main/java/inu/codin/codin/common/schedular/exception/SchedulerException.java
@@ -0,0 +1,7 @@
+package inu.codin.codin.common.schedular.exception;
+
+public class SchedulerException extends RuntimeException{
+    public SchedulerException(String message){
+        super(message);
+    }
+}
