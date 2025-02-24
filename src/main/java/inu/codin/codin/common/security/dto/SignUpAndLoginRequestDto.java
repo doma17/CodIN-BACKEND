100588abfd4267fd469b60b9652d3b5d6174f050
X1n9fU <minkyeong0244@gmail.com>
2025-02-24 17:40:28 +0900

    fix : admin, test 계정을 위한 login api 생성

--- src/main/java/inu/codin/codin/common/security/dto/SignUpAndLoginRequestDto.java
+++ src/main/java/inu/codin/codin/common/security/dto/SignUpAndLoginRequestDto.java
@@ -1,7 +1,7 @@
 package inu.codin.codin.common.security.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,8 +10,8 @@ import lombok.Setter;
 public class SignUpAndLoginRequestDto {
 
     @NotBlank
-    @Size(min = 9 , max = 9 , message = "학번은 9자리여야 합니다.")
-    private String studentId;
+    @Email
+    private String email;
 
     @NotBlank
     private String password;
--- src/main/java/inu/codin/codin/common/security/service/AuthService.java
+++ src/main/java/inu/codin/codin/common/security/service/AuthService.java
@@ -2,6 +2,7 @@ package inu.codin.codin.common.security.service;
 
 import inu.codin.codin.common.Department;
 import inu.codin.codin.common.exception.NotFoundException;
+import inu.codin.codin.common.security.dto.SignUpAndLoginRequestDto;
 import inu.codin.codin.common.security.enums.AuthResultStatus;
 import inu.codin.codin.domain.user.dto.request.UserProfileRequestDto;
 import inu.codin.codin.domain.user.entity.UserEntity;
@@ -148,102 +149,9 @@ public class AuthService {
 
 
 
-//
-//    public Integer signUp(SignUpAndLoginRequestDto signUpAndLoginRequestDto, HttpServletResponse response){
-//        try {//학번으로 회원가입 유무 판단
-//            Optional<UserEntity> user = userRepository.findByStudentId(signUpAndLoginRequestDto.getStudentId());
-//            if (user.isPresent()) {
-//                UsernamePasswordAuthenticationToken authenticationToken
-//                        = new UsernamePasswordAuthenticationToken(signUpAndLoginRequestDto.getStudentId(), signUpAndLoginRequestDto.getPassword());
-//
-//                Authentication authentication = authenticationManager.authenticate(authenticationToken);
-//                SecurityContextHolder.getContext().setAuthentication(authentication);
-//
-//                jwtService.createToken(response);
-//                return 0;
-//            }
-//            PortalLoginResponseDto userPortalLoginResponseDto
-//                    = returnPortalInfo(signUpAndLoginRequestDto);
-//            redisAuthService.saveUserData(signUpAndLoginRequestDto.getStudentId(), userPortalLoginResponseDto);
-//            return 1;
-//        } catch (Exception e) {
-//            log.error("로그인 중 오류 발생", e);
-//            throw new UserCreateFailException("아이디 혹은 비밀번호를 잘못 입력하였습니다.");
-//        }
-//
-//    }
-//
-//    private PortalLoginResponseDto returnPortalInfo(SignUpAndLoginRequestDto signUpAndLoginRequestDto) throws Exception {
-//        String html = portalClient.signUp(
-//                "submit",
-//                AESUtil.encrypt(signUpAndLoginRequestDto.getStudentId()),
-//                AESUtil.encrypt(signUpAndLoginRequestDto.getPassword())
-//        );
-//        Document doc = Jsoup.parse(html);
-//        String value = doc.select(".main").select("input[type=hidden]").attr("value");
-//        PortalLoginResponseDto userPortalLoginResponseDto = readJson(value);
-//        String password = passwordEncoder.encode(signUpAndLoginRequestDto.getPassword());
-//        userPortalLoginResponseDto.setPassword(password);
-//        userPortalLoginResponseDto.setUndergraduate(isUnderGraduate(signUpAndLoginRequestDto));
-//        return userPortalLoginResponseDto;
-//    }
-//
-//    private PortalLoginResponseDto readJson(String value) {
-//        String[] arrayList = value.substring(1).split(", ");
-//        PortalLoginResponseDto userPortalLoginResponseDto = new PortalLoginResponseDto();
-//
-//        Map<String, Consumer<String>> fieldSetters = Map.of(
-//                "studId", userPortalLoginResponseDto::setStudentId,
-//                "userNm", userPortalLoginResponseDto::setName,
-//                "userEml", userPortalLoginResponseDto::setEmail,
-//                "userDpmtNm", v -> userPortalLoginResponseDto.setDepartment(Department.fromDescription(v)),
-//                "userCollNm", userPortalLoginResponseDto::setCollege
-//        );
-//
-//        for (String user : arrayList) {
-//            String[] info = user.split("=");
-//            if (info.length == 2 && fieldSetters.containsKey(info[0])) { // Only process known fields
-//                fieldSetters.get(info[0]).accept(info[1]);
-//            }
-//        }
-//
-//        return userPortalLoginResponseDto;
-//    }
-//
-//
-//    public boolean isUnderGraduate(@Valid SignUpAndLoginRequestDto signUpAndLoginRequestDto){
-//        String basic = "Basic " + Base64.getEncoder().encodeToString((signUpAndLoginRequestDto.getStudentId() + ":" + signUpAndLoginRequestDto.getPassword()).getBytes(ISO_8859_1));
-//        Map<String, String> graduate = inuClient.status(basic);
-//        return graduate.get("undergraduate").equals("true");
-//    }
-//
-//    public void createUser(String studentId, UserNicknameRequestDto userNicknameRequestDto, MultipartFile userImage) {
-//
-//        Optional<UserEntity> nickNameDuplicate = userRepository.findByNicknameAndDeletedAtIsNull(userNicknameRequestDto.getNickname());
-//        if (nickNameDuplicate.isPresent()){
-//            throw new UserNicknameDuplicateException("이미 사용중인 닉네임입니다.");
-//        }
-//        PortalLoginResponseDto userData = redisAuthService.getUserData(studentId);
-//        log.info("[createUser] 요청 데이터: {}", studentId);
-//
-//        String imageUrl = null;
-//        if (userImage != null) {
-//            log.info("[회원가입] 프로필 이미지 업로드 중...");
-//            imageUrl = s3Service.handleImageUpload(List.of(userImage)).get(0);
-//            log.info("[회원가입] 프로필 이미지 업로드 완료: {}", imageUrl);
-//        }
-//
-//        // imageUrl이 null이면 기본 이미지로 설정
-//        if (imageUrl == null) {
-//            imageUrl = s3Service.getDefaultProfileImageUrl(); // S3Service에서 기본 이미지 URL 가져오기
-//
-//        }
-//
-//        UserEntity user = UserEntity.of(userData);
-//        user.updateNickname(userNicknameRequestDto);
-//        user.updateProfileImageUrl(imageUrl);
-//        userRepository.save(user);
-//
-//        log.info("[signUpUser] SIGN UP SUCCESS!! : {}", user.getStudentId());
-//    }
+
+    public void login(SignUpAndLoginRequestDto signUpAndLoginRequestDto, HttpServletResponse response){
+
+    }
+
 }
