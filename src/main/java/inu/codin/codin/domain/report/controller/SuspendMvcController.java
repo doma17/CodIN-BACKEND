065ea63604897c1bba03cc9a1ea5014931f1d146
X1n9fU <minkyeong0244@gmail.com>
2025-03-06 02:22:36 +0900

    feat : 정지 유저에게 보여줄 화면 구성 및 MVC 처리 (새로운 화면이 생기기 전까지 임시 화면)

---
+++ src/main/java/inu/codin/codin/domain/report/controller/SuspendMvcController.java
@@ -0,0 +1,24 @@
+package inu.codin.codin.domain.report.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Controller
+@RequestMapping("/suspends")
+public class SuspendMvcController {
+
+    @GetMapping
+    public String getSuspendedView(@RequestParam("endDate") String endDate, Model model){
+        LocalDateTime dateTime = LocalDateTime.parse(endDate);
+        LocalDateTime adjustedDateTime = dateTime.plusHours(1);
+        String formattedDate = adjustedDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH시"));
+        model.addAttribute("endDate", formattedDate);
+        return "suspends";
+    }
+}
