91bb3507f75da1dde92cff2542ca6bc8a706f7f0
X1n9fU <minkyeong0244@gmail.com>
2025-03-19 14:29:19 +0900

    fix : 채팅방 UNSUBSCRIBE, DISCONNECT 부분을 분기

--- src/main/java/inu/codin/codin/common/stomp/StompMessageProcessor.java
+++ src/main/java/inu/codin/codin/common/stomp/StompMessageProcessor.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageDeliveryException;
+import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.messaging.support.MessageHeaderAccessor;
@@ -29,20 +30,25 @@ public class StompMessageProcessor implements ChannelInterceptor {
         if (headerAccessor == null || headerAccessor.getCommand() == null){
             throw new MessageDeliveryException(HttpStatus.BAD_REQUEST.toString());
         }
+        /*
+         구독 취소인 경우에는 chatRoomId가 존재하면 방을 나가는 것
+         연결 끊기는 세션에서 채팅방 나가기 및 제외하는 로직 추가
+         */
+        if (headerAccessor.getCommand() == StompCommand.UNSUBSCRIBE || headerAccessor.getCommand() == StompCommand.DISCONNECT) {
+            boolean hasChatRoomId = headerAccessor.getSessionAttributes() != null
+                    && headerAccessor.getSessionAttributes().containsKey("chatRoomId");
 
-        switch (headerAccessor.getCommand()){
-            case CONNECT -> {
-                stompMessageService.connectSession(headerAccessor);
-            }
-            case SUBSCRIBE -> {
-                stompMessageService.enterToChatRoom(headerAccessor);
-            }
-            case UNSUBSCRIBE -> {
+            if (hasChatRoomId) {
                 stompMessageService.exitToChatRoom(headerAccessor);
             }
-            case DISCONNECT -> {
-                stompMessageService.exitToChatRoom(headerAccessor);
+            if (headerAccessor.getCommand() == StompCommand.DISCONNECT) {
                 stompMessageService.disconnectSession(headerAccessor);
+            }
+
+        } else if (headerAccessor.getDestination() != null && headerAccessor.getDestination().matches("/queue(/unread)?/[^/]+")) {
+            switch (headerAccessor.getCommand()) {
+                case CONNECT -> stompMessageService.connectSession(headerAccessor);
+                case SUBSCRIBE -> stompMessageService.enterToChatRoom(headerAccessor);
             }
         }
     }
