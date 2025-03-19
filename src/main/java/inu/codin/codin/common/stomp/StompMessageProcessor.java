d42aa7fc384abfdbbf8b1daa3409bbf407958139
X1n9fU <minkyeong0244@gmail.com>
2025-03-19 22:17:24 +0900

    fix : UNSUBSCRIBE, DISCONNECT의 역할을 완전히 분기

--- src/main/java/inu/codin/codin/common/stomp/StompMessageProcessor.java
+++ src/main/java/inu/codin/codin/common/stomp/StompMessageProcessor.java
@@ -6,11 +6,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageDeliveryException;
-import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
@@ -30,26 +31,16 @@ public class StompMessageProcessor implements ChannelInterceptor {
         if (headerAccessor == null || headerAccessor.getCommand() == null){
             throw new MessageDeliveryException(HttpStatus.BAD_REQUEST.toString());
         }
-        /*
-         구독 취소인 경우에는 chatRoomId가 존재하면 방을 나가는 것
-         연결 끊기는 세션에서 채팅방 나가기 및 제외하는 로직 추가
-         */
-        if (headerAccessor.getCommand() == StompCommand.UNSUBSCRIBE || headerAccessor.getCommand() == StompCommand.DISCONNECT) {
-            boolean hasChatRoomId = headerAccessor.getSessionAttributes() != null
-                    && headerAccessor.getSessionAttributes().containsKey("chatRoomId");
 
-            if (hasChatRoomId) {
-                stompMessageService.exitToChatRoom(headerAccessor);
+        switch (headerAccessor.getCommand()) {
+            case CONNECT -> stompMessageService.connectSession(headerAccessor);
+            case SUBSCRIBE -> {
+                if (Objects.requireNonNull(headerAccessor.getDestination()).matches("/queue(/unread)?/[^/]+"))
+                    //헤더에 chatRoomId가 필요한 destination
+                    stompMessageService.enterToChatRoom(headerAccessor);
             }
-            if (headerAccessor.getCommand() == StompCommand.DISCONNECT) {
-                stompMessageService.disconnectSession(headerAccessor);
-            }
-
-        } else if (headerAccessor.getDestination() != null && headerAccessor.getDestination().matches("/queue(/unread)?/[^/]+")) {
-            switch (headerAccessor.getCommand()) {
-                case CONNECT -> stompMessageService.connectSession(headerAccessor);
-                case SUBSCRIBE -> stompMessageService.enterToChatRoom(headerAccessor);
-            }
+            case UNSUBSCRIBE -> stompMessageService.exitToChatRoom(headerAccessor); //채팅방에서 끊긴 상태
+            case DISCONNECT -> stompMessageService.disconnectSession(headerAccessor);  //stomp 세션 끊기
         }
     }
 
