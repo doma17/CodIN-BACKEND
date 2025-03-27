97d790b51a6bf41281f6de8abcbf44ad9aca8903
X1n9fU <minkyeong0244@gmail.com>
2025-03-28 02:24:48 +0900

    perf: Cache에 베스트 게시글 데이터를 놓고 점수 반영 시 실시간으로 업데이트 #187

--- src/main/java/inu/codin/codin/infra/redis/service/RedisBestService.java
+++ src/main/java/inu/codin/codin/infra/redis/service/RedisBestService.java
@@ -1,7 +1,10 @@
 package inu.codin.codin.infra.redis.service;
 
 
+import inu.codin.codin.domain.post.domain.best.BestEntity;
+import inu.codin.codin.domain.post.domain.best.BestRepository;
 import inu.codin.codin.domain.post.domain.hits.service.HitsService;
+import inu.codin.codin.infra.redis.config.RedisHealthChecker;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
@@ -11,10 +14,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
@@ -29,14 +29,18 @@ public class RedisBestService {
      * 24시간 실시간 반영으로 3위까지 나타냄
      */
     private final RedisTemplate<String, String> redisTemplate;
+    private final RedisHealthChecker redisHealthChecker;
+
     private final HitsService hitsService;
+    private final BestRepository bestRepository;
+    private final String BEST_KEY = "post:top3";
 
     /**
      * best 게시글을 반환하는 순간으로부터 24시간 동안의 가장 score가 높은 게시글 N개를 반환
      * @param N 순위
      * @return N개의 Key : postId, Value : score 의 score 기준 내림차순 Map
      */
-    public Map<String, Double> getTopNPosts(int N) {
+    public Map<String, Double> delicatedBestsScheduler(int N) {
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd/HH");
 
@@ -44,12 +48,14 @@ public class RedisBestService {
         Map<String, Double> result = new HashMap<>();
         for (int i = 0; i < 24; i++) {
             String redisKey = now.minusHours(i).format(formatter);
-            Set<ZSetOperations.TypedTuple<String>> members = redisTemplate.opsForZSet().reverseRangeWithScores(redisKey, 0, - 1);
-            if (members != null) {
-                for (ZSetOperations.TypedTuple<String> member :members){
-                    String postId = member.getValue();
-                    Double score = member.getScore();
-                    result.put(postId, score);
+            if (Boolean.TRUE.equals(redisTemplate.hasKey(redisKey))) {
+                Set<ZSetOperations.TypedTuple<String>> members = redisTemplate.opsForZSet().reverseRangeWithScores(redisKey, 0, -1);
+                if (members != null) {
+                    for (ZSetOperations.TypedTuple<String> member : members) {
+                        String postId = member.getValue();
+                        Double score = member.getScore();
+                        result.put(postId, score);
+                    }
                 }
             }
         }
@@ -73,7 +79,33 @@ public class RedisBestService {
     }
 
     /**
-     * 24시간 내에 해당 게시글의 score가 존재하면 점수 반영
+     * Cache에서 저장 중인 Best 게시글 정보 정렬 및 반환
+     * @return Key : PostId (게시글_id) - Value : Score (점수)
+     */
+    public Map<String, Double> getBests(){
+        if (redisHealthChecker.isRedisAvailable()){
+            Set<ZSetOperations.TypedTuple<String>> members = redisTemplate.opsForZSet().rangeWithScores(BEST_KEY, 0, - 1);
+            if (members!=null && !members.isEmpty())
+                return members.stream()
+                        .sorted((e1, e2) -> {
+                            //1. 점수 기준 내림차순
+                            int scoreComparison = Double.compare(Double.parseDouble(String.valueOf(e2.getScore())), Double.parseDouble(String.valueOf(e1.getScore())));
+
+                            // 2. 점수가 동일하면 조회수 기준 내림차순 정렬
+                            return scoreComparison != 0 ? scoreComparison :
+                                    Integer.compare(hitsService.getHitsCount(new ObjectId(e2.getValue())),
+                                            hitsService.getHitsCount(new ObjectId(e1.getValue())));
+                        }).collect(Collectors.toMap(
+                                ZSetOperations.TypedTuple::getValue,
+                                ZSetOperations.TypedTuple::getScore,
+                                (existing, replacement) -> existing,
+                                LinkedHashMap::new
+                        ));
+        }
+        return null;
+    }
+    /**
+     * Cache에 저장된 24시간 데이터 중 해당 게시글의 score가 존재하면 점수 반영
      * 만약 새로운 게시글이라면 현재 시간대로 새로 생성하여 게시글 점수 저장
      * @param score 게시글에 더할 점수
      * @param postId 게시글 _id
@@ -83,20 +115,97 @@ public class RedisBestService {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd/HH");
 
         String redisKey;
-        //이전 24시간대에서 해당 게시글의 score를 찾기
-        for (int i=0; i<24; i++){
-            redisKey = now.minusHours(i).format(formatter);
-            if (Boolean.TRUE.equals(redisTemplate.hasKey(redisKey))) {
-                Double scoreOfBest = redisTemplate.opsForZSet().score(redisKey, postId.toString());
-                if (scoreOfBest != null) { //score가 존재하면 업데이트
-                    redisTemplate.opsForZSet().incrementScore(redisKey, postId.toString(), score);
-                    return;
+        if (redisHealthChecker.isRedisAvailable()) {
+            //이전 24시간대에서 해당 게시글의 score를 찾기
+            for (int i = 0; i < 24; i++) {
+                redisKey = now.minusHours(i).format(formatter);
+                if (Boolean.TRUE.equals(redisTemplate.hasKey(redisKey))) {
+                    Double scoreOfBest = redisTemplate.opsForZSet().score(redisKey, postId.toString());
+                    if (scoreOfBest != null) { //score가 존재하면 업데이트
+                        redisTemplate.opsForZSet().incrementScore(redisKey, postId.toString(), score);
+                        updateBests(redisKey, postId.toString());
+                        return;
+                    }
                 }
             }
+            //score가 존재하지 않으면 현재 시간대로 새로 생성
+            redisKey = now.format(formatter);
+            redisTemplate.expire(redisKey, 1, TimeUnit.DAYS); //하루가 지나면 필요없는 데이터
+            redisTemplate.opsForZSet().add(redisKey, postId.toString(), score);
+            updateBests(redisKey, postId.toString());
         }
-        //score가 존재하지 않으면 현재 시간대로 새로 생성
-        redisKey = now.format(formatter);
-        redisTemplate.expire(redisKey, 1, TimeUnit.DAYS);
-        redisTemplate.opsForZSet().add(redisKey, postId.toString(), score);
+    }
+
+    /**
+     * 게시글에 점수가 반영 될 때마다 베스트 게시글을 관리하는 ZSet에서 업데이트
+     * 베스트 게시글에서 최소 점수보다 같거나 크거나, 최소 점수가 없거나, 베스트 게시글이 3개 이하거나 할 때 베스트 게시글에 포함
+     *
+     * 만약 포함 후 3개 초과라면
+     * 새로 적용된 게시글의 점수와 최소 점수와 같으면 조회수로 판단
+     * 상위 3개를 제외한 나머지 삭제 후 DB 저장
+     */
+    private void updateBests(String redisKey, String postId){
+        Double score = redisTemplate.opsForZSet().score(redisKey, postId);
+        Set<ZSetOperations.TypedTuple<String>> minEntry = redisTemplate.opsForZSet().rangeWithScores(BEST_KEY, 0, 0);
+
+        if (minEntry!=null && !minEntry.isEmpty()){
+            ZSetOperations.TypedTuple<String> minTuple = minEntry.iterator().next(); //최소 score의 Tuple
+            Double min = minTuple.getScore();
+            Long totalSize = redisTemplate.opsForZSet().size(BEST_KEY);
+
+            //최소 점수가 없거나, 최소 점수보다 같거나 큰 값이거나, 총 베스트 게시글 개수가 3개 미만 이면 포함
+            if (min == null || score >= min || totalSize < 3)
+                redisTemplate.opsForZSet().add(BEST_KEY, postId, score);
+
+            totalSize = redisTemplate.opsForZSet().size(BEST_KEY);
+            if (totalSize > 3) { //3개가 넘어가면
+                if (score.equals(min)) //만약 최소값끼리 같으면 조회수로 비교
+                    checkHits(postId, minTuple.getValue());
+                else redisTemplate.opsForZSet().removeRange(BEST_KEY, 0, totalSize-4); //상위 3개를 제외하고 삭제
+            }
+
+            Set<ZSetOperations.TypedTuple<String>> members = redisTemplate.opsForZSet().rangeWithScores(BEST_KEY, 0, - 1);
+            members.forEach(member -> saveBests(member.getValue(), member.getScore().intValue()));
+        }
+
+
+    }
+
+    /**
+     * 베스트 게시글에 처음 적용된 게시글과 기존에 있던 최소 점수 게시글의 조회수 비교
+     * @param postId 점수가 적용된 게시글 _id
+     * @param minPostId 베스트 게시글 중에서 가장 최소 점수를 가진 게시글 _id
+     */
+    private void checkHits(String postId, String minPostId) {
+        if (hitsService.getHitsCount(new ObjectId(minPostId)) > hitsService.getHitsCount(new ObjectId(postId))) {
+            redisTemplate.opsForZSet().remove(BEST_KEY, postId);
+        } else {
+            redisTemplate.opsForZSet().remove(BEST_KEY, minPostId);
+        }
+
+    }
+
+    /**
+     * 만약 bestEntity에 없다면 저장
+     */
+    public void saveBests(String postId, int score) {
+        boolean existedPost = bestRepository.existsByPostId(new ObjectId(postId));
+        if (!existedPost) {
+            bestRepository.save(BestEntity.builder()
+                    .postId(new ObjectId(postId))
+                    .score(score)
+                    .build());
+        }
+    }
+
+    /**
+     * 동기화 과정에서 기존에 있던 값을 지우고 새롭게 등록
+     * 스케줄러를 통해 매 시마다 전 1시간 동안의 베스트 게시글을 등록
+     */
+    public void resetBests(Map<String, Double> posts) {
+        if (Boolean.TRUE.equals(redisTemplate.hasKey(BEST_KEY))) {
+            redisTemplate.opsForZSet().removeRange(BEST_KEY, 0 ,-1);
+        }
+        posts.forEach((key, value) -> redisTemplate.opsForZSet().add(BEST_KEY, key, value));
     }
 }
