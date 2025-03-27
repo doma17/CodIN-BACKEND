4fa8917490a7682868c9d770f70ac13ea3f88a6e
X1n9fU <minkyeong0244@gmail.com>
2025-03-28 02:27:09 +0900

    refactor : Redis에 의존하는 데이터 감소에 따른 스케줄러 수정 #187

--- src/main/java/inu/codin/codin/infra/redis/scheduler/SyncScheduler.java
+++ src/main/java/inu/codin/codin/infra/redis/scheduler/SyncScheduler.java
@@ -1,49 +1,22 @@
 package inu.codin.codin.infra.redis.scheduler;
 
-import inu.codin.codin.domain.lecture.domain.review.entity.ReviewEntity;
-import inu.codin.codin.domain.lecture.domain.review.repository.ReviewRepository;
-import inu.codin.codin.domain.like.entity.LikeEntity;
-import inu.codin.codin.domain.like.entity.LikeType;
-import inu.codin.codin.domain.like.repository.LikeRepository;
-import inu.codin.codin.domain.post.domain.best.BestEntity;
-import inu.codin.codin.domain.post.domain.best.BestRepository;
-import inu.codin.codin.domain.post.domain.comment.entity.CommentEntity;
-import inu.codin.codin.domain.post.domain.comment.repository.CommentRepository;
-import inu.codin.codin.domain.post.domain.reply.entity.ReplyCommentEntity;
-import inu.codin.codin.domain.post.domain.reply.repository.ReplyCommentRepository;
-import inu.codin.codin.domain.post.entity.PostEntity;
-import inu.codin.codin.domain.post.repository.PostRepository;
 import inu.codin.codin.infra.redis.config.RedisHealthChecker;
-import inu.codin.codin.infra.redis.service.RedisLikeService;
-import inu.codin.codin.infra.redis.service.RedisService;
+import inu.codin.codin.infra.redis.service.RedisBestService;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class SyncScheduler {
 
-    private final PostRepository postRepository;
-    private final CommentRepository commentRepository;
-    private final ReplyCommentRepository replyCommentRepository;
-    private final ReviewRepository reviewRepository;
-
-    private final LikeRepository likeRepository;
-    private final BestRepository bestRepository;
-
-    private final RedisService redisService;
-    private final RedisLikeService redisLikeService;
+    private final RedisBestService redisBestService;
     private final RedisHealthChecker redisHealthChecker;
 
     @Async
@@ -53,96 +26,23 @@ public class SyncScheduler {
             log.warn("Redis 비활성화 상태, 동기화 작업 중지");
             return;
         }
-        log.info(" 동기화 작업 시작");
-        syncEntityLikes("POST", postRepository);
-        syncEntityLikes("COMMENT", commentRepository);
-        syncEntityLikes("REPLY", replyCommentRepository);
-        syncEntityLikes("REVIEW", reviewRepository);
-        log.info(" 동기화 작업 완료");
+//        log.info(" 동기화 작업 시작");
+//        syncEntityLikes("POST", postRepository);
+//        syncEntityLikes("COMMENT", commentRepository);
+//        syncEntityLikes("REPLY", replyCommentRepository);
+//        syncEntityLikes("REVIEW", reviewRepository);
+//        log.info(" 동기화 작업 완료");
     }
-
-    private <T> void syncEntityLikes(String entityType, MongoRepository<T, ObjectId> repository) {
-        Set<String> redisKeys = redisService.getKeys(entityType+ ":likes:*")
-                .stream().map(redisKey -> redisKey.replace(entityType+ ":likes:", ""))
-                .collect(Collectors.toSet());
-        if (redisKeys.isEmpty()) return;
-
-        LikeType likeType = LikeType.valueOf(entityType);
-
-        // 좋아요 대상 _id 와 좋아요 entity 리스트
-        List<LikeEntity> dbLikes = likeRepository.findByLikeTypeAndDeletedAtIsNull(likeType);
-        Map<String, List<LikeEntity>> dbLikeMap = dbLikes.stream()
-                .collect(Collectors.groupingBy(likeEntity -> likeEntity.getLikeTypeId().toString()));
-
-        //DB에 있지만 Redis에 없는 좋아요 복구
-        dbLikeMap.forEach((entityId, likes) -> {
-            if (!redisKeys.contains(entityId))
-                likes.forEach(like -> redisLikeService.addLike(entityType, like.getLikeTypeId(), like.getUserId()));
-        });
-
-        //Redis에 있지만 DB에 없는 좋아요 삭제
-        for (String likeTypeId : redisKeys) {
-            Set<String> redisUsers = redisLikeService.getLikedUsers(entityType, likeTypeId);
-            Set<String> dbUsers = dbLikeMap.getOrDefault(likeTypeId, List.of()).stream()
-                    .map(like -> like.getUserId().toString())
-                    .collect(Collectors.toSet());
-
-            ObjectId entityTypeId = new ObjectId(likeTypeId);
-
-            redisUsers.stream().filter(userId -> !dbUsers.contains(userId))
-                    .forEach(userId -> {
-                        log.info("[Redis] 좋아요 삭제: UserID={}, LikeID={}", userId, likeTypeId);
-                        redisLikeService.removeLike(entityType, entityTypeId, new ObjectId(userId));
-                    });
-
-            // likeCount 업데이트
-            int likeCount = redisLikeService.getLikeCount(entityType, new ObjectId(likeTypeId));
-            if (repository instanceof PostRepository postRepo) {
-                PostEntity post = postRepo.findByIdAndNotDeleted(entityTypeId).orElse(null);
-                if (post != null && post.getLikeCount() != likeCount) {
-                    log.info("PostEntity 좋아요 수 업데이트: EntityID={}, Count={}", entityTypeId, likeCount);
-                    post.updateLikeCount(likeCount);
-                    postRepo.save(post);
-                }
-            } else if (repository instanceof CommentRepository commentRepo) {
-                CommentEntity comment = commentRepo.findByIdAndNotDeleted(entityTypeId).orElse(null);
-                if (comment != null && comment.getLikeCount() != likeCount) {
-                    log.info("CommentEntity 좋아요 수 업데이트: EntityID={}, Count={}", entityTypeId, likeCount);
-                    comment.updateLikeCount(likeCount);
-                    commentRepo.save(comment);
-                }
-            } else if (repository instanceof ReplyCommentRepository replyRepo) {
-                ReplyCommentEntity reply = replyRepo.findByIdAndNotDeleted(entityTypeId).orElse(null);
-                if (reply != null && reply.getLikeCount() != likeCount) {
-                    log.info("ReplyEntity 좋아요 수 업데이트: EntityID={}, Count={}", entityTypeId, likeCount);
-                    reply.updateLikeCount(likeCount);
-                    replyRepo.save(reply);
-                }
-            } else if (repository instanceof ReviewRepository reviewRepo) {
-                ReviewEntity review = reviewRepo.findBy_idAndDeletedAtIsNull(entityTypeId).orElse(null);
-                if (review != null && review.getLikeCount() != likeCount) {
-                    log.info("ReviewEntity 좋아요 수 업데이트: EntityID={}, Count={}", entityTypeId, likeCount);
-                    review.updateLikeCount(likeCount);
-                    reviewRepo.save(review);
-                }
-            }
+    @Async
+    @PostConstruct
+    @Scheduled(cron = "0 0 * * * ?") // 1시간 마다 실행
+    public void getTop3BestPosts() {
+        if (redisHealthChecker.isRedisAvailable()) {
+            Map<String, Double> posts = redisBestService.delicatedBestsScheduler(3);
+            redisBestService.resetBests(posts);
+            posts.forEach((key, value) -> redisBestService.saveBests(key, value.intValue()));
         }
     }
 
-    @Scheduled(fixedRate = 43200000) // 12시간 마다 실행
-    public void getTop3BestPosts() {
-        Map<String, Double> posts = redisService.getTopNPosts(3);
-        posts.entrySet().stream()
-                .peek(post -> {
-                    BestEntity bestPost = bestRepository.findByPostId(new ObjectId(post.getKey()));
-                    if (bestPost == null) {
-                        bestRepository.save(BestEntity.builder()
-                                .postId(new ObjectId(post.getKey()))
-                                .score(post.getValue().intValue())
-                                .build());
-                    }
-                }
-        );
-    }
 
 }
