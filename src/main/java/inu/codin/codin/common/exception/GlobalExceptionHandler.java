3ef8b1f6113625fd5650a406e08a61591aa9c13f
X1n9fU <minkyeong0244@gmail.com>
2025-03-31 00:55:56 +0900

    fix : RedisSystemException, OAuth2AuthenticationException 에러 핸들러에 추가

--- src/main/java/inu/codin/codin/common/exception/GlobalExceptionHandler.java
+++ src/main/java/inu/codin/codin/common/exception/GlobalExceptionHandler.java
@@ -3,13 +3,17 @@ package inu.codin.codin.common.exception;
 import inu.codin.codin.common.response.ExceptionResponse;
 import inu.codin.codin.common.security.exception.JwtException;
 import inu.codin.codin.domain.chat.chatroom.exception.ChatRoomExistedException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.RedisSystemException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
@@ -35,14 +39,27 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ExceptionResponse> handleValidationExceptions(MethodArgumentNotValidException e) {
+    protected ResponseEntity<ExceptionResponse> handleValidationExceptions(MethodArgumentNotValidException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(new ExceptionResponse(e.getBindingResult().getFieldErrors().get(0).getDefaultMessage(), HttpStatus.BAD_REQUEST.value()));    }
 
     @ExceptionHandler(ChatRoomExistedException.class)
-    public ResponseEntity<ExceptionResponse> handleChatRoomExistedException(ChatRoomExistedException e) {
+    protected ResponseEntity<ExceptionResponse> handleChatRoomExistedException(ChatRoomExistedException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(new ExceptionResponse(e.getMessage() +"/"+ e.getChatRoomId(), e.getErrorCode()));
+    }
+
+    @ExceptionHandler(RedisSystemException.class)
+    public ResponseEntity<ExceptionResponse> handleRedisSystemException(RedisSystemException e){
+        log.error(e.getMessage(), e.getStackTrace()[0]);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ExceptionResponse(e.getMessage(), HttpStatus.BAD_REQUEST.value()));
+    }
+
+    @ExceptionHandler(OAuth2AuthenticationException.class)
+    protected ResponseEntity<ExceptionResponse> handleOAuth2AuthenticationException(OAuth2AuthenticationException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(new ExceptionResponse(e.getMessage(), HttpStatus.UNAUTHORIZED.value()));
     }
 
 }
