6ca6971e79736dc2b8c55574984c5d700a85ce49
X1n9fU <minkyeong0244@gmail.com>
2025-03-31 21:56:33 +0900

    perf : 로그인 Fail 시 login 화면으로 redirect 및 error Param 추가

--- src/main/java/inu/codin/codin/common/security/util/OAuth2LoginFailureHandler.java
+++ src/main/java/inu/codin/codin/common/security/util/OAuth2LoginFailureHandler.java
@@ -6,10 +6,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.OAuth2Error;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -18,8 +20,11 @@ import java.io.PrintWriter;
 @Component
 @Slf4j
 @RequiredArgsConstructor
-public class OAuth2LoginFailureHandler implements AuthenticationFailureHandler {
-    private final ObjectMapper objectMapper;
+public class OAuth2LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Value("${server.domain}")
+    private String BASEURL;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request,
@@ -29,10 +34,12 @@ public class OAuth2LoginFailureHandler implements AuthenticationFailureHandler {
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 
         String errorMessage = "인증 실패.";
+        String errorCode = null;
         if (exception instanceof OAuth2AuthenticationException oauth2Ex) {
             OAuth2Error error = oauth2Ex.getError();
             if (error != null && error.getDescription() != null) {
                 errorMessage = error.getDescription();
+                errorCode = error.getErrorCode();
             }
         }
 
@@ -41,5 +48,10 @@ public class OAuth2LoginFailureHandler implements AuthenticationFailureHandler {
 
         log.error("[OAuth2LoginFailureHandler] {}", responseBody);
         response.getWriter().write(responseBody);
+        if (errorCode == null)
+            getRedirectStrategy().sendRedirect(request, response, BASEURL+"/login?");
+        else
+            getRedirectStrategy().sendRedirect(request, response, BASEURL+"/login?error="+errorCode);
+
     }
 }
