9bfa775e1449bdcf6e5ed1774025527f3526ecf4
kbm <rovin1273@gmail.com>
2025-06-24 21:59:24 +0900

    test: Email Service 테스트 추가
    아래와 같은 세가지에 대해서 테스트를 작성했습니다.
    - EmailTemplateServiceTest
    - JoinEmailAuthServiceTest
    - PasswordResetEmailServiceTest
    
    테스트시 객체 생성 메서드가 없기에 두개의 DTO 클래스에 Builder를 추가했습니다.
    - JoinEmailCheckRequestDto
    - JoinEmailSendRequestDto

--- src/main/java/inu/codin/codin/domain/email/dto/JoinEmailCheckRequestDto.java
+++ src/main/java/inu/codin/codin/domain/email/dto/JoinEmailCheckRequestDto.java
@@ -3,6 +3,7 @@ package inu.codin.codin.domain.email.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Getter;
 
 /**
@@ -10,6 +11,7 @@ import lombok.Getter;
  * 해당 이메일로 전송된 인증 코드를 확인한다.
  */
 @Getter
+@Builder
 public class JoinEmailCheckRequestDto {
 
     @Schema(description = "이메일 주소", example = "example@inu.ac.kr")
