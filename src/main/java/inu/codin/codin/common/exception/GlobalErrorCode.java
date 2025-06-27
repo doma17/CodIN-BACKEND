1d54470fea4f17d390995d74a9974cf5a88f7457
X1n9fU <minkyeong0244@gmail.com>
2025-06-27 23:44:23 +0900

    fix : GlobalException, ErrorCode interface 설정. ChatException 제작

---
+++ src/main/java/inu/codin/codin/common/exception/GlobalErrorCode.java
@@ -0,0 +1,9 @@
+package inu.codin.codin.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+public interface GlobalErrorCode {
+    HttpStatus httpStatus();
+
+    String message();
+}
