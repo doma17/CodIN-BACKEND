90795f3530191412b6d444a42dd98edd9358c21f
X1n9fU <minkyeong0244@gmail.com>
2025-07-01 02:18:49 +0900

    fix : Scheme 작성 및 Department의 JSON 매핑 로직 추가

--- src/main/java/inu/codin/codin/domain/info/entity/PartnerImg.java
+++ src/main/java/inu/codin/codin/domain/info/entity/PartnerImg.java
@@ -1,11 +1,16 @@
 package inu.codin.codin.domain.info.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
 public class PartnerImg {
+
+    @Schema(description = "제휴업체 메인 이미지", example = "https://example.com")
     private String main;
+
+    @Schema(description = "제휴업체 서브 이미지", example = "[\"https://example.com\", \"https://example.com\"]")
     private List<String> sub;
 }
--- src/main/java/inu/codin/codin/domain/info/exception/InfoErrorCode.java
+++ src/main/java/inu/codin/codin/domain/info/exception/InfoErrorCode.java
@@ -10,7 +10,9 @@ public enum InfoErrorCode implements GlobalErrorCode {
     LAB_NOT_FOUND(HttpStatus.NOT_FOUND, "LAB 정보를 찾을 수 없습니다."),
 
     PROFESSOR_NOT_FOUND(HttpStatus.NOT_FOUND, "PROFESSOR 정보를 찾을 수 없습니다."),
-    PROFESSOR_DUPLICATED(HttpStatus.CONFLICT, "이미 존재하는 PROFESSOR 정보 입니다.");
+    PROFESSOR_DUPLICATED(HttpStatus.CONFLICT, "이미 존재하는 PROFESSOR 정보 입니다."),
+
+    PARTNER_NOT_FOUND(HttpStatus.NOT_FOUND, "PARTNER 정보를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
